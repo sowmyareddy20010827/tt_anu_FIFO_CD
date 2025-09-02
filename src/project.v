554c497f328dce5a9636ad62de406763e9431c87
sowmya <pothulasowmya408@gmail.com>
2025-09-01 20:39:02 -0700

    updated make file

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_anudeesh_cdc_fifo (
+module tt_um_anu_fifo_cdc (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
